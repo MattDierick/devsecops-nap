ef09cfff4c52926572707a281b9d525b9591991f
MattDierick <matt262810@gmail.com>
2022-04-01 11:25:52 +0200

    push2

--- pull-git.sh
+++ pull-git.sh
@@ -1,2 +1,3 @@
 git clone https://github.com/MattDierick/devsecops-nap.git /tmp/devsecops/
-cp -r /tmp/devsecops/etc/nginx/ /etc/nginx/
+cp -r /tmp/devsecops/etc/nginx/* /etc/nginx/
+echo "fdp" > /tmp/devsecops/echo-test.txt
