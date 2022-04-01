be1d0929615fdf76f7f3037636642a3dff638d0e
MattDierick <matt262810@gmail.com>
2022-04-01 18:35:51 +0200

    puzrtfg

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "kubernetes_deployment" "nginx-nap" {
           app = "nginx-nap"
         }
         annotations = {
-          version = "v1.5"
+          version = "v1.7"
         }
       }
 
