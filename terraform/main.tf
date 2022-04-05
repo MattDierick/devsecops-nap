b1f1556963b36c4e925226c5b8977e1fc10d00a3
MattDierick <matt262810@gmail.com>
2022-04-05 18:08:47 +0200

    violations

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "kubernetes_deployment" "nginx-nap" {
           app = "nginx-nap"
         }
         annotations = {
-          version = "v1.41"
+          version = "v1.42"
         }
       }
 
