3294bd25af9a364e6051be84745c56327ec8715c
MattDierick <matt262810@gmail.com>
2022-04-05 14:04:18 +0200

    bot2

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "kubernetes_deployment" "nginx-nap" {
           app = "nginx-nap"
         }
         annotations = {
-          version = "v1.27"
+          version = "v1.28"
         }
       }
 
