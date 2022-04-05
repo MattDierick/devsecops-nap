b9b2310704c0b39f91013324e3132e1b801662a0
MattDierick <matt262810@gmail.com>
2022-04-05 18:05:50 +0200

    ref

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "kubernetes_deployment" "nginx-nap" {
           app = "nginx-nap"
         }
         annotations = {
-          version = "v1.40"
+          version = "v1.41"
         }
       }
 
