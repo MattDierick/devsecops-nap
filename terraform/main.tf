60ce4932e32a7b4bdc03c18d3b2300b9ac146b11
MattDierick <matt262810@gmail.com>
2022-04-07 11:20:15 +0200

    cahnge fqdn

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "kubernetes_deployment" "nginx-nap" {
           app = "nginx-nap"
         }
         annotations = {
-          version = "v1.52"
+          version = "v1.53"
         }
       }
 
