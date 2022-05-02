62e722d5520d4a0640147d37596958a9000bdf8a
Matthieu DIERICK <matt262810@gmail.com>
2022-05-02 20:15:49 +0200

    Update outputs.tf

--- terraform/prep-env/outputs.tf
+++ terraform/prep-env/outputs.tf
@@ -1,18 +1,22 @@
-
- output "client_key" {
+output "client_key" {
    value = azurerm_kubernetes_cluster.default.kube_config.0.client_key
    sensitive = true
- }
+}
 
- output "client_certificate" {
+output "client_certificate" {
    value = azurerm_kubernetes_cluster.default.kube_config.0.client_certificate
    sensitive = true
- }
+}
 
- output "cluster_ca_certificate" {
+output "cluster_ca_certificate" {
    value = azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate
    sensitive = true
- }
+}
+
+output "cluster_fqdn" {
+   value = azurerm_kubernetes_cluster.default.kube_config.0.fqdn
+   sensitive = true
+}
 
 output "kube_config" {
   value = azurerm_kubernetes_cluster.default.kube_config_raw
